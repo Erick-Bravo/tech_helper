88f28024565b4e31231538794cf7fb98665590a0
Erick-Bravo <erickbrvo@gmail.com>
2023-10-07 13:55:58 -0700

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Westinghouse from './pages/Westinghouse';
-import HTP from './pages/HTP';
-import { Box } from '@chakra-ui/react';
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Westinghouse from "./pages/Westinghouse";
+import HTP from "./pages/HTP";
+import { Box } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 const Home = () => {
-  return (
-    <Box>This is Home</Box>
-  )
-}
+  return <Box>This is Home</Box>;
+};
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/westinghouse" element={<Westinghouse />} />
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,46 @@
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
+
+const navTexPad = "0 20px";
+const darkGray = "#474747"
+const offWhite = "#E2E2E2"
+
+const NavBar = () => {
+  return (
+    <Flex
+      w="100%"
+      p="0 100px"
+      h="50px"
+      backgroundColor={darkGray}
+      justifyContent="space-between"
+      alignItems="center"
+      borderBottom="2px"
+      color="white"
+    >
+      <Flex justifyContent="flex-start">
+        <Text p="0 20px" fontSize="25px" fontWeight="bold">
+          Tech Helper
+        </Text>
+      </Flex>
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        h="100%"
+        fontWeight="bold"
+        fontFamily="sans-serif"
+        color="white"
+      >
+        <Link p={navTexPad} _hover={{textUnderlineOffset: "none"}} href="/">
+          Home
+        </Link>
+        <Link p={navTexPad} _hover={{textUnderlineOffset: "none"}} href="/htp">
+          HTP
+        </Link>
+        <Link p={navTexPad} _hover={{textUnderlineOffset: "none"}} href="/westinghouse">
+          Westingthouse
+        </Link>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default NavBar;
