d8c3b63caa0fb5ace02eea6cf789cc4398ef87a2
Erick-Bravo <erickbrvo@gmail.com>
2023-10-15 20:26:55 -0700

    handle enter. refactoring

---
+++ src/components/ModelSearch.tsx
@@ -0,0 +1,82 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Link,
+  Text,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { modelList, modelListTypes } from "../data/modelList";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+
+const linkColor = "#4895EF";
+
+const ModelSearch = () => {
+  const [inputText, setInput] = useState("");
+  const [result, setResult] = useState<modelListTypes[]>([
+    { id: "Model", name: "Name", status: "Status", link: "" },
+  ]);
+
+  const handleSubmit = () => {
+    const filteredList = modelList.filter((item) => {
+      if (inputText === "") {
+        return;
+      }
+      return item.id === inputText;
+    });
+    setResult(filteredList);
+  };
+
+  const modelInput = (event: any) => {
+    const text = event.target.value.toUpperCase();
+    setInput(text);
+  };
+
+  const handleEnter = (event: { key: string }) => {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  return (
+    <FormControl>
+      <FormLabel>Model Search</FormLabel>
+      <Input
+        placeholder="Model"
+        backgroundColor="white"
+        w="500px"
+        border="1px solid black"
+        m="20px"
+        h="50px"
+        onChange={modelInput}
+        onKeyDown={handleEnter}
+      />
+      <Button w="100px" mb="20px" onClick={handleSubmit}>
+        Search
+      </Button>
+      {result.length === 0 && <Text fontSize="35px">No result found</Text>}
+      {result.map((item) => {
+        return (
+          <Box key={item.id}>
+            <Text
+              fontSize="35px"
+              m="20px 10px"
+            >{` ${item.id} - ${item.name} - ${item.status}`}</Text>
+            {item.link !== "" && (
+              <Link href={item.link} color={linkColor} fontSize="20" isExternal>
+                Model Links Page <ExternalLinkIcon mx="2px" />
+              </Link>
+            )}
+          </Box>
+        );
+      })}
+    </FormControl>
+  );
+};
+
+export default ModelSearch;
--- src/pages/HTP.tsx
+++ src/pages/HTP.tsx
@@ -1,32 +1,15 @@
-import { Box, Button, Flex, Input, Link, Text } from "@chakra-ui/react";
-import { useState } from "react";
-import { modelList, modelListTypes } from "../data/modelList";
-import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Box, Button, Flex, Input, Link, Text,   FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText, } from "@chakra-ui/react";
+import ModelSearch from "../components/ModelSearch";
 
 const offWhite = "#E2E2E2";
 const linkColor = "#4895EF";
 
+// Input field needs to be a FORM, so "enter" button will work. (Less hand movements)
+
 const HTP = () => {
-  const [inputText, setInput] = useState("");
-  const [result, setResult] = useState<modelListTypes[]>([
-    { id: "Model", name: "Name", status: "Status", link: "" },
-  ]);
-
-  const modelInput = (e: { target: { value: string } }) => {
-    const text = e.target.value.toUpperCase();
-    setInput(text);
-  };
-
-  const handleSubmit = () => {
-    const filteredList = modelList.filter((item) => {
-      if (inputText === "") {
-        return;
-      }
-      return item.id === inputText;
-    });
-    setResult(filteredList);
-  };
-
   return (
     <Flex
       backgroundColor={offWhite}
@@ -35,35 +18,7 @@ const HTP = () => {
       alignItems="center"
       flexDirection="column"
     >
-      <Input
-        placeholder="Model"
-        backgroundColor="white"
-        w="500px"
-        border="1px solid black"
-        m="20px"
-        h="50px"
-        onChange={modelInput}
-      />
-      <Button w="100px" mb="20px" onClick={handleSubmit}>
-        Search
-      </Button>
-      {result.length === 0 && <Text fontSize="35px">No result found</Text>}
-      {result.map((item) => {
-        return (
-          <>
-            <Text
-              fontSize="35px"
-              m="10px"
-              key={item.id}
-            >{` ${item.id} - ${item.name} - ${item.status}`}</Text>
-            {item.link !== "" && (
-              <Link href={item.link} color={linkColor} fontSize="20" isExternal>
-                Model Links Page <ExternalLinkIcon mx='2px' />
-              </Link>
-            )}
-          </>
-        );
-      })}
+      <ModelSearch />
     </Flex>
   );
 };
