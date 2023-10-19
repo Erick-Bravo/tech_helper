768ca3e11bee3bb4a8227bb0ce25c724c5adcd5a
Erick-Bravo <erickbrvo@gmail.com>
2023-10-18 23:00:15 -0700

    localStorage partially complete

--- src/components/HomeSwitchComps/ToDo.tsx
+++ src/components/HomeSwitchComps/ToDo.tsx
@@ -1,9 +1,39 @@
-
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Box,
+  Button,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const ToDo = () => {
+
+  //For New To Do. Add to existing localHost data.
+  //Push to existing localGet
+  const [name, setName] = useState("");
+  const [ticket, setTicket] = useState("");
+  const [description, setDescription] = useState("");
+
+
+  const localGet = JSON.parse(localStorage.getItem("toDoObject") || "")
+  console.log(localGet)
+
   return (
-   <div>Create New To Do</div>
-  )
-}
+    <Box>
+      <FormControl>
+        <FormLabel>Name</FormLabel>
+        <Input type="text" w="300px" bg="white" />
+        <FormLabel>Ticket</FormLabel>
+        <Input type="number" w="300px" bg="white" />
+        <FormLabel>Description</FormLabel>
+        <Input type="text" w="300px" bg="white" />
+      </FormControl>
+      <Button mt="30px">Submit</Button>
+    </Box>
+  );
+};
 
 export default ToDo;
