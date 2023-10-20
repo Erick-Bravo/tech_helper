c58e8517d7ade6863e5813db4c23c02a76274f50
Erick-Bravo <erickbrvo@gmail.com>
2023-10-19 21:53:25 -0700

    json parse and local storage removed. values for inputs added.

--- src/components/HomeSwitchComps/ToDo.tsx
+++ src/components/HomeSwitchComps/ToDo.tsx
@@ -1,35 +1,43 @@
-import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  Input,
-  Box,
-  Button,
-} from "@chakra-ui/react";
+import { FormControl, FormLabel, Input, Box, Button } from "@chakra-ui/react";
 import { useState } from "react";
 
 const ToDo = () => {
-
   //For New To Do. Add to existing localHost data.
   //Push to existing localGet
   const [name, setName] = useState("");
   const [ticket, setTicket] = useState("");
   const [description, setDescription] = useState("");
 
-
-  const localGet = JSON.parse(localStorage.getItem("toDoObject") || "")
-  console.log(localGet)
+  // const localGet = JSON.parse(localStorage.getItem("toDoObject") || "")
+  // console.log(localGet)
 
   return (
     <Box>
       <FormControl>
         <FormLabel>Name</FormLabel>
-        <Input type="text" w="300px" bg="white" />
+        <Input
+          type="text"
+          w="300px"
+          bg="white"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
         <FormLabel>Ticket</FormLabel>
-        <Input type="number" w="300px" bg="white" />
+        <Input
+          type="number"
+          w="300px"
+          bg="white"
+          onChange={(e) => setTicket(e.target.value)}
+          value={ticket}
+        />
         <FormLabel>Description</FormLabel>
-        <Input type="text" w="300px" bg="white" />
+        <Input
+          type="text"
+          w="300px"
+          bg="white"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
       </FormControl>
       <Button mt="30px">Submit</Button>
     </Box>
