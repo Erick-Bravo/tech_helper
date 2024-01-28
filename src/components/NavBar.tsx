82eefe35251fb47f29b3393b6f75def45523665b
Erick-Bravo <erickbrvo@gmail.com>
2024-01-28 11:49:45 -0800

    sit parts files link. w/loading cursor

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,28 @@
 import { Box, Flex, Link, Text } from "@chakra-ui/react";
 import { darkGray } from "../globals";
+import { useState } from "react";
 
 const navTexPad = "0 20px";
 
-const excelFileId =
+const warrantyPartsID =
   "https://aristonthermo.sharepoint.com/:x:/r/sites/USAWarrantyShipments-SAP/Documenti%20condivisi/General/Warranty%20Parts%20Shipping.xlsx?d=wdd52a2b1fbb145629b47a222ecc8ede3&csf=1&web=1&e=IwmqUj";
-const teamsLink = `msteams://teams.microsoft.com/l/file/${excelFileId}`;
+const sitPartsID =
+  "https://aristonthermo.sharepoint.com/:x:/r/sites/AristoCustomerExperienceTeam-TechSupport/Documenti%20condivisi/Tech%20Support/SiT%20Programmer%20Software/SIT%20Part%20Files-9-27-2022.xlsx?d=waffffd07b2914887a1c421c6b8b12098&csf=1&web=1&e=BxSaX7";
+
+const teamsWarrantyPartsLink = `msteams://teams.microsoft.com/l/file/${warrantyPartsID}`;
+const teamsSITLink = `msteams://teams.microsoft.com/l/file/${sitPartsID}`;
 
 const NavBar = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleCurserLoading = () => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  };
+
   return (
     <Flex
       w="100%"
@@ -38,13 +53,21 @@ const NavBar = () => {
         <Link
           p={navTexPad}
           _hover={{ textUnderlineOffset: "none" }}
-          href={teamsLink}
+          href={teamsWarrantyPartsLink}
+          onClick={handleCurserLoading}
+          cursor={loading ? "wait" : "pointer"}
         >
           Warranty Parts Shipping
         </Link>
-        {/* <Link p={navTexPad} _hover={{textUnderlineOffset: "none"}} href="/westinghouse">
-          Westingthouse
-        </Link> */}
+        <Link
+          p={navTexPad}
+          _hover={{ textUnderlineOffset: "none" }}
+          href={teamsSITLink}
+          onClick={handleCurserLoading}
+          cursor={loading ? "wait" : "pointer"}
+        >
+          SIT Program Files
+        </Link>
       </Flex>
     </Flex>
   );
