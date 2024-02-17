32519d0e011437da6142659248e743724e612aea
Erick-Bravo <erickbrvo@gmail.com>
2024-02-17 14:00:53 -0800

    initial creation and placement on TimeZoneBar

---
+++ src/components/TimeZoneBar.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+
+
+const TimeZoneBar = () => {
+  return (
+   <Box w="100%" h="50px">This is the TimeZoneBar component</Box>
+  )
+}
+
+export default TimeZoneBar;
