3b95e32c94614f329a9a1666c5d3279bd3c6475d
Erick-Bravo <erickbrvo@gmail.com>
2024-02-17 21:30:49 -0800

    time is working for all time zones. refreshes every minute

--- src/components/TimeZoneBar.tsx
+++ src/components/TimeZoneBar.tsx
@@ -1,10 +1,62 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
+const ZoneText: React.FC<{ text: string; timeZone: string }> = ({
+  text,
+  timeZone,
+}) => {
+  const [currentTime, setCurrentTime] = useState<string>("");
+
+  useEffect(() => {
+    const updateCurrentTime = () => {
+      const date = new Date();
+      const newTime = date.toLocaleTimeString("en-US", {
+        timeZone: timeZone,
+        hour12: true,
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+      const [hours, minutes] = newTime.split(":");
+      const formattedTime = `${hours}:${minutes}`;
+      setCurrentTime(formattedTime);
+    };
+
+    updateCurrentTime();
+    const intervalId = setInterval(updateCurrentTime, 60000);
+
+    return () => clearInterval(intervalId);
+  }, [timeZone]);
+
+  return (
+    <Box>
+      <Text fontWeight="bold">{text}</Text>
+      <Text fontWeight="bold" fontSize="20px" color="#555555">
+        {currentTime}
+      </Text>
+    </Box>
+  );
+};
 
 const TimeZoneBar = () => {
+  const WestZone = "America/Los_Angeles";
+  const MountainZone = "America/Denver";
+  const CentralZone = "America/Chicago";
+  const EastZone = "America/New_York";
+
   return (
-   <Box w="100%" h="50px">This is the TimeZoneBar component</Box>
-  )
-}
+    <Flex
+      w="100%"
+      h="70px"
+      justifyContent="space-around"
+      alignItems="center"
+      bg="lightgray"
+    >
+      <ZoneText text="West: " timeZone={WestZone} />
+      <ZoneText text="Mountain: " timeZone={MountainZone} />
+      <ZoneText text="Central: " timeZone={CentralZone} />
+      <ZoneText text="East: " timeZone={EastZone} />
+    </Flex>
+  );
+};
 
 export default TimeZoneBar;
